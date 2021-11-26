32a64eda64b75bd5edd955467362e4108c115a0e
조성모 커뮤니케이션 <1929207@hyundaidept.com>
2021-11-26 12:53:52 +0900

    DRM 설정 서버 수정 및 배치 추가

---
+++ batch/drm_mail_server.bat
@@ -0,0 +1,4 @@
+cd C:\Users\Administrator\rpa_naver_brandmall
+python drm_mail_server.py
+pause
+exit
--- drm_mail_server.py
+++ drm_mail_server.py
@@ -1,34 +1,42 @@
 import os
-from flask import Flask  # 서버 구현을 위한 Flask 객체 import
+from flask import Flask, request  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 from flask_cors import CORS
-from comm import naver
-from comm import mail
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 CORS(app)
 api = Api(app)  # Flask 객체에 Api 객체 등록
-
-path = 'C:\Program Files (x86)\freeFTPd\sftproot\rpa\'
-@api.route('/sendmail/<string:params>')
+# SFTP 서버에서 파일을 받기로 설정된 폴더 경로를 적어줘야함
+path = r'C:\Users\Administrator\Desktop\drm_need\rpa'
+@api.route('/sendmail')
 class SendMail(Resource):
-    def get(self, params):
+    def post(self):
         to = ["chosm10@kakao.com", "chosm10@hyundai-ite.com", "cindy@hyundaihmall.com", "move@hyundai-ite.com"]
-        #서버에서 받을 때 첫번째는 받는 사람, 두번째는 메일 제목, 세번째는 메일 내용, 네번째부터 파일 이름
-        params = params.split(' ')
-        to.append(params[0])
-        subject = params[1]
-        msg = params[2]
-        files = params[3:]
+        # to.append(request.json.get('to'))
+        subject = request.json.get('subject')
+        msg = request.json.get('msg')
+        files = request.json.get('files')
 
         attach = []
         for file in files:
-            filepath = r'{}{}'.format(path, file)
-            naver.setDRM(filepath)
+            filepath = r'{}\{}'.format(path, file)
             attach.append(filepath)
+            if 'csv' in file:
+                continue
 
-        mail.sendmail(to, subject, msg, attach)
+            try:
+                naver.setDRM(filepath)
+            except Exception as e:
+                print('{} drm 설정 실패: {}'.format(filepath, e))
         
-
+        try:
+            subject = '테스트메일 입니다.'
+            mail.sendmail(to, subject, msg, attach)
+            print('메일 발송 성공')
+        except Exception as e:
+            print('메일 발송 실패: {}'.format(e))
 if __name__ == "__main__":
+
+    from comm import naver
+    from comm import mail
     app.run(debug=True, host='0.0.0.0', port=9000)
